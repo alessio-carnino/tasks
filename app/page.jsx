a62a058211b4c1409c7e2071631e380ee16c0cfa
Alessio <carninoalessio@gmail.com>
2024-04-26 16:08:43 +0200

    End project - Still Errors to be fixed

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import TicketCard from "./(components)/TicketCard";
 
-const Dashboard = () => {
-  return <div>Dashboard </div>;
+const getTickets = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/Tickets", {
+      cache: "no-store",
+    });
+
+    return res.json();
+  } catch (error) {
+    console.log("Failed to get tickts", error);
+  }
+};
+
+const Dashboard = async () => {
+  const { tickets } = await getTickets();
+
+  // for every ticket we are grabbing the Category and pass it to Set() which is going to remove all the duplicates from the list
+  const uniqueCategories = [
+    ...new Set(tickets?.map(({ category }) => category)),
+  ];
+
+  return (
+    <div className="p-5">
+      <div>
+        {tickets &&
+          uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+            <div key={categoryIndex} className="mb-4">
+              <h2>{uniqueCategory}</h2>
+              <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+                {tickets
+                  .filter((ticket) => ticket.category === uniqueCategory)
+                  .map((filteredTicket, _index) => (
+                    <TicketCard
+                      id={_index}
+                      key={_index}
+                      ticket={filteredTicket}
+                    />
+                  ))}
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
